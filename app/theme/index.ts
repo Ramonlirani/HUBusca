a5b4bc5141be0485b92f22d2dc589f0ed1cdb3a9
ramonlirani <ramonlirani@hotmail.com>
2023-11-11 04:26:34 -0300

    feat: added new colors to the theme

--- app/theme/index.ts
+++ app/theme/index.ts
@@ -3,10 +3,14 @@ export default {
       gray: "#333",
       primary: "#22272E",
       secondary: "#373E47",
+      third: "#1C2128",
 
       white: "#ffffff",
       white_2: "#fafbfc",
       white_3: "#F5F5F5",
+
+      text_primary: "#ADBAC7",
+      text_secondary: "#5E6A75",
 
       blue: "#263040",
       blue_text: "#5097ED",
