a121ffcdaaa3a58df67f6a674697d445c1e54861
ramonlirani <ramonlirani@hotmail.com>
2023-11-11 04:28:32 -0300

    feat: user interface created

---
+++ app/types/user.ts
@@ -0,0 +1,11 @@
+export interface UserProps {
+   name: string;
+   login: string;
+   followers: number;
+   following: number;
+   id: number;
+   location: string;
+   avatar_url: string;
+   repository_public: number;
+   repository_url: string;
+}
