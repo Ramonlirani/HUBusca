7b23d5b444d4180db4c62d3e194e90c41a4b95ca
ramonlirani <ramonlirani@hotmail.com>
2023-11-10 21:52:49 -0300

    feat: the theme was created and added to app.tsx and the navigation container was added along with the routes folder.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "styled-components";
+
+import { MyStack } from "./app/routes/MyStack";
+import theme from "./app/theme";
+import { StatusBar } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <ThemeProvider theme={theme}>
+         <NavigationContainer>
+            <MyStack />
+            <StatusBar backgroundColor="transparent" translucent />
+         </NavigationContainer>
+      </ThemeProvider>
+   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/theme/index.ts
@@ -0,0 +1,18 @@
+export default {
+   colors: {
+      gray: "#333",
+      primary: "#22272E",
+      secondary: "#373E47",
+
+      white: "#ffffff",
+      white_2: "#fafbfc",
+      white_3: "#F5F5F5",
+
+      blue: "#263040",
+      blue_text: "#5097ED",
+
+      green: "#2dba4e",
+
+      purple: "#6e5494",
+   },
+};
