2d8197b232fcacbde8de34fb8895ad0198e8c2e2
ramonlirani <ramonlirani@hotmail.com>
2023-11-13 01:40:13 -0300

    feat: Added colors per programming language to the theme

--- app/theme/index.ts
+++ app/theme/index.ts
@@ -14,9 +14,37 @@ export default {
 
       blue: "#263040",
       blue_text: "#5097ED",
-
-      green: "#2dba4e",
-
-      purple: "#6e5494",
    },
+};
+
+export const getColorForLanguage = (language: string) => {
+   switch (language) {
+      case "JavaScript":
+         return "#F1E05A";
+      case "TypeScript":
+         return "#3178C6";
+      case "Java":
+         return "#b07219";
+      case "HTML":
+         return "#E34C26";
+      case "CSS":
+         return "#563D7C";
+      case "YAML":
+         return "#CB171E";
+      case "PHP":
+         return "#4F5D95";
+      case "Go":
+         return "#00ADD8";
+      case "C++":
+         return "#F34B7D";
+      case "Python":
+         return "#3572A5";
+      case "Ruby":
+         return "#701516";
+      case "Dart":
+         return "#00B4AB";
+
+      default:
+         return "#ADBAC7";
+   }
 };
