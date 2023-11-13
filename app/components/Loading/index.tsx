0d8737ca3d60348f47ff5225e3cecfe11d5a004c
ramonlirani <ramonlirani@hotmail.com>
2023-11-13 01:35:35 -0300

    feat: creation of the loading component using react-native ActivityIndicator

---
+++ app/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { ActivityIndicator } from "react-native";
+import { Container, LoadingStyle } from "./styles";
+
+export function Loading() {
+   return (
+      <Container>
+         <LoadingStyle />
+      </Container>
+   );
+}
