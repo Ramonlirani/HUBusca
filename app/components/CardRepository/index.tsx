36d7e35d6bc83d219e1ffe7b14d850e258fcd20c
ramonlirani <ramonlirani@hotmail.com>
2023-11-13 01:31:21 -0300

    feat: creation of the cardRepository component and its style

---
+++ app/components/CardRepository/index.tsx
@@ -0,0 +1,90 @@
+import { FlatList, Linking } from "react-native";
+import {
+   CardContainer,
+   DescriptionText,
+   NameText,
+   DataRepoContainer,
+   TittleText,
+   PublicText,
+   HeaderText,
+   CircleColorTech,
+   TechText,
+   CreateAtText,
+   DataCreateAtText,
+   CreateAtContainer,
+   TechContainer,
+   LastCommitText,
+   DataLastCommitText,
+   LastCommitContainer,
+   TouchableOpacityContainer,
+} from "./styles";
+import { formatDate } from "../DateFormatter";
+import { getColorForLanguage } from "../../theme";
+
+interface Repository {
+   id: number;
+   name: string;
+   html_url: string;
+   description: string;
+   language: string;
+   created_at: string;
+   pushed_at: string;
+}
+
+interface CardRepositoryProps {
+   repositoryList: Repository[];
+}
+
+export const CardRepository = ({ repositoryList }: CardRepositoryProps) => {
+   const handlePress = (url: string) => {
+      Linking.openURL(url);
+   };
+
+   return (
+      <>
+         <TittleText>Popular repositories</TittleText>
+         <FlatList
+            data={repositoryList}
+            showsVerticalScrollIndicator={true}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+               <TouchableOpacityContainer
+                  onPress={() => handlePress(item.html_url)}
+               >
+                  <CardContainer>
+                     <HeaderText>
+                        <NameText>{item.name}</NameText>
+                        <PublicText>Public</PublicText>
+                     </HeaderText>
+                     <DescriptionText>{item.description}</DescriptionText>
+                     <DataRepoContainer>
+                        <TechContainer>
+                           <CircleColorTech
+                              style={{
+                                 backgroundColor: getColorForLanguage(
+                                    item.language
+                                 ),
+                              }}
+                           />
+                           <TechText>{item.language}</TechText>
+                        </TechContainer>
+                        <CreateAtContainer>
+                           <CreateAtText>CreateAt:</CreateAtText>
+                           <DataCreateAtText>
+                              {formatDate(item.created_at)}
+                           </DataCreateAtText>
+                        </CreateAtContainer>
+                     </DataRepoContainer>
+                     <LastCommitContainer>
+                        <LastCommitText>Last Commit:</LastCommitText>
+                        <DataLastCommitText>
+                           {formatDate(item.pushed_at)}
+                        </DataLastCommitText>
+                     </LastCommitContainer>
+                  </CardContainer>
+               </TouchableOpacityContainer>
+            )}
+         />
+      </>
+   );
+};
---
+++ app/components/CardRepository/styles.ts
@@ -0,0 +1,133 @@
+import styled from "styled-components/native";
+
+export const TouchableOpacityContainer = styled.TouchableOpacity`
+   background-color: ${({ theme }) => theme.colors.primary};
+   align-items: center;
+   justify-items: center;
+`;
+
+export const CardContainer = styled.View`
+   width: 80%;
+   flex-direction: column;
+   background-color: ${({ theme }) => theme.colors.primary};
+   border-radius: 6px;
+   border-width: 0.5px;
+   border-color: white;
+   padding: 10px;
+   margin-bottom: 20px;
+`;
+
+export const TittleText = styled.Text`
+   padding: 40px;
+   padding-bottom: 10px;
+   font-size: 16px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_primary};
+   font-family: monospace;
+`;
+
+export const HeaderText = styled.View`
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+export const PublicText = styled.Text`
+   font-size: 11px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_primary};
+   font-family: monospace;
+   border-width: 0.5px;
+   border-radius: 10px;
+   border-color: ${({ theme }) => theme.colors.text_primary};
+   padding: 2px;
+   padding-right: 4px;
+   padding-left: 4px;
+`;
+
+export const NameText = styled.Text`
+   font-size: 14px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.blue_text};
+   font-family: monospace;
+`;
+
+export const DescriptionText = styled.Text`
+   margin-top: 6px;
+   font-size: 12px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_secondary};
+   font-family: monospace;
+`;
+
+export const DataRepoContainer = styled.View`
+   flex-direction: row;
+   align-items: center;
+   margin-top: 10px;
+   justify-content: space-between;
+`;
+
+export const TechContainer = styled.View`
+   flex-direction: row;
+   align-items: center;
+`;
+
+export const CircleColorTech = styled.View`
+   width: 14px;
+   height: 14px;
+   margin-top: 6px;
+   background-color: #b07219;
+   border-radius: 7px;
+`;
+
+export const TechText = styled.Text`
+   margin-left: 8px;
+   margin-top: 6px;
+   font-size: 14px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_secondary};
+   font-family: monospace;
+`;
+
+export const CreateAtContainer = styled.View`
+   flex-direction: row;
+`;
+
+export const CreateAtText = styled.Text`
+   margin-left: 18px;
+   margin-top: 6px;
+   font-size: 12px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_primary};
+   font-family: monospace;
+`;
+
+export const DataCreateAtText = styled.Text`
+   margin-top: 6px;
+   font-size: 12px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_secondary};
+   font-family: monospace;
+`;
+
+export const LastCommitContainer = styled.View`
+   flex-direction: row;
+   justify-content: flex-end;
+`;
+
+export const LastCommitText = styled.Text`
+   margin-left: 18px;
+   margin-top: 6px;
+   font-size: 12px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_primary};
+   font-family: monospace;
+`;
+
+export const DataLastCommitText = styled.Text`
+   margin-top: 6px;
+   font-size: 12px;
+   font-weight: normal;
+   color: ${({ theme }) => theme.colors.text_secondary};
+   font-family: monospace;
+`;
